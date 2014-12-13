ba2938fbc4de09abc56d39e19f765b1274c60735
ranicza <ranicza@gmail.com>
2014-12-13 11:17:37 +0300

    with trigger

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,74 @@
+var Contact = {
+	firstName: '',
+	lastName: '',
+	addresses: [],
+	phones: [],
+	emails: [],
+	fullName: function(){
+		return this.firstName + ' ' + this.lastName;
+	},
+	valid: function(){
+		if(this.firstName.length == 0 || this.firstName == undefined || this.lastName.length == 0 || this.lastName == undefined){
+			alert("Enter your contact correctly!");
+			return false;
+		}else return true;
+	}
+};
+
+var Address = {
+	street: '',
+	city: '',
+	state: '',
+	fullAddress: function(){
+		return this.street + this.city + this.state;
+	},
+	valid: function(){
+		if(this.street.length == 0 || this.city.length == 0 || this.state.length ==0 || this.street == undefined ||
+			this.city == undefined || this.state == undefined){
+			alert("Enter your address correctly!");
+			return false;
+		}else{
+			return true;
+		}
+	}
+};
+
+var Phone = {
+	phone: '',
+	workPhone: '',
+	cellPhone: '',
+	phoneFormat: function() {
+		var number = this.valid();
+		return "(+" + number.slice(0, 3) + " " + number.slice(3, 5) + " " + number.slice(5,8) + "-"+
+			number.slice(8, 10) + "-" + number.slice(10, 12) + ")";
+	},
+	valid: function() {
+		var number = this.phone.replace(/[^\d]/g,'');
+		if (number.length == 12) {
+			return number;
+		} else {
+			alert("Enter your phone number correctly!");
+			return false;
+		}
+	}
+};
+
+var Email = {
+	mail: '',
+	workMail: '',
+	cellMail: '',
+	valid: function(){
+		//var reg = /\S+@\S+\.\S+/;
+		var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(reg.test(this.mail)){
+			return reg.test(this.mail);
+		}else{
+			alert("Enter your email correctly!");
+		}
+	}
+};
+
+
 $(document).ready(function(){
 	$('#add-address').click(function(){
 		$("#new-addresses").append('<div class= "has-success new-address">' + '<p>Another address</p>'+'<div class = "form-group">' +
@@ -22,43 +93,36 @@ $(document).ready(function(){
 		var inputFirstName = $("input#new-first-name").val();
 		var inputLastName = $('input#new-last-name').val();
 
-		var newContact = {
-			firstName: inputFirstName,
-			lastName: inputLastName,
-			addresses: [],
-			phones: [],
-			emails: [],
-			fullName: function(){
-				return this.firstName + ' ' + this.lastName;
-			}
-		};
+		var newContact = Object.create(Contact);
+		newContact.firstName = inputFirstName;
+		newContact.lastName = inputLastName;
+
 
 		$('.new-address').each(function(){
 			var inputtedStreet = $(this).find('input#new_street').val();
 			var inputtedCity = $(this).find("input#new_city").val();
 			var inputtedState = $(this).find("input#new_state").val();
 
-			var newAddress = {
-				street: inputtedStreet,
-				city: inputtedCity,
-				state: inputtedState,
-				fullAddress: function(){
-					return this.street + this.city + this.state;
-				}
-			};
+			var newAddress = Object.create(Address);
+			newAddress.city = inputtedCity;
+			newAddress.street = inputtedStreet;
+			newAddress.state = inputtedState;
 			newContact.addresses.push(newAddress);
 		});
 
 		$('.new-phones').each(function(){
 			var inputtedPhone = $(this).find('input#new_phone').val();
-			var newPhones = {phone: inputtedPhone};
-			newContact.phones.push(newPhones);
+
+			var newPhone = Object.create(Phone);
+			newPhone.phone = inputtedPhone;
+			newContact.phones.push(newPhone);
 		});
 
 		$('.new-mail').each(function(){
 			var inputtedEmail = $(this).find('input#new_email').val();
-			var newEmails = {mail: inputtedEmail};
-			newContact.emails.push(newEmails);
+			var newEmail = Object.create(Email);
+			newEmail.mail = inputtedEmail;
+			newContact.emails.push(newEmail);
 		});
 
 		$("ul#contacts").append("<li><span class='contact'>"+ newContact.fullName()  + "</span></li>");
